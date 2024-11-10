75745fff5b9bdd783c157b8e2b70631f191ef68c
wynston0921 <66952452+wynston0921@users.noreply.github.com>
2024-11-10 22:04:51 +0800

    Update food.kt

--- app/src/main/java/com/example/foodOrderAndTrackingApp/food.kt
+++ app/src/main/java/com/example/foodOrderAndTrackingApp/food.kt
@@ -1,13 +1,18 @@
 package com.example.foodOrderAndTrackingApp
 
-class Food{
-  private val foodList: MutableList<Food> = mutableListOf()
+class Food(
+    val name: String,
+    val category: String,
+    val price: Int
+) {
+    private val foodList: MutableList<Food> = mutableListOf()
 
-  fun addFood(name:String, category:String, price:int){
-    var food = Food(name, category, price)
-    foodList.add(food)
-  }
-fun getAllFoods(); List<food> {
-  return foodList
-  }
+    fun addFood(name: String, category: String, price: Int) {
+        val food = Food(name, category, price)
+        foodList.add(food)
+    }
+
+    fun getAllFoods(): List<Food> {  
+        return foodList
+    }
 }
