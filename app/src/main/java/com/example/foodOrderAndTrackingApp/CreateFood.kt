57e06a8c3bdbcbf1f003b1193863fa2a7a8591b3
wynston0921 <66952452+wynston0921@users.noreply.github.com>
2024-11-10 21:33:52 +0800

    Update CreateFood.kt

--- app/src/main/java/com/example/foodOrderAndTrackingApp/CreateFood.kt
+++ app/src/main/java/com/example/foodOrderAndTrackingApp/CreateFood.kt
@@ -14,15 +14,38 @@ class CreateFood : AppCompatActivity() {
     private lateinit var edtCategory: EditText
     private lateinit var edtPrice: EditText
     private lateinit var btnAddFood: Button
+    private val food = Food()
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_create_food)
+
+        edtName = findViewById(R.id.edt_name)
+        edtCategory = findViewById(R.id.edt_category)
+        edtCategory = findViewById(R.id.edt_price)
+        edtCategory = findViewById(R.id.btn_add_price)
+        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        btnAddFood.setOnClickListener{
+            addFood
+        }
+    }
+    private fun addFood(){
+        val name= edtName.text.toString()
+        val category = edtCategory.text.toString()
+        val price = edt[rice.text.toString().toDoubleOrNull()
+
+        if(name.isNotEmpty() && category.isNotEmpty() && price != null) {
+            food.addFood(name, category, price)
+            Toast.makeText(this,"food add successful",Toast.LENGTH_SHORT).show()
+            finish()
+        }else{
+            Toast.makeText(this,"plase input food",LENGTH_SHORT).show()
         }
     }
 }
