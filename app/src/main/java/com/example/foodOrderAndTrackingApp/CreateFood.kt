765a646fc29b1d15437f0149a53127dc453086ae
wynston0921 <66952452+wynston0921@users.noreply.github.com>
2024-11-10 21:11:04 +0800

    Update CreateFood.kt

--- app/src/main/java/com/example/foodOrderAndTrackingApp/CreateFood.kt
+++ app/src/main/java/com/example/foodOrderAndTrackingApp/CreateFood.kt
@@ -1,12 +1,20 @@
 package com.example.foodOrderAndTrackingApp
 
 import android.os.Bundle
+import andorid.widget.Button
+import android.widget.EditText
+import andorid.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class CreateFood : AppCompatActivity() {
+    private lateinit var edtName: EditText
+    private lateinit var edtCategory: EditText
+    private lateinit var edtPrice: EditText
+    private lateinit var btnAddFood: Button
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
