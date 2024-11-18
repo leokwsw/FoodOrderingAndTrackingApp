de03f0445a05214e9152c33673603e739027565c
Leo Wu <leo.wu.kw@gmail.com>
2024-11-18 16:42:47 +0800

    login

--- app/src/main/java/com/example/foodOrderAndTrackingApp/LoginActivity.kt
+++ app/src/main/java/com/example/foodOrderAndTrackingApp/LoginActivity.kt
@@ -2,10 +2,15 @@ package com.example.foodOrderAndTrackingApp
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.addTextChangedListener
 import com.google.android.material.button.MaterialButton
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -13,7 +18,10 @@ import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var auth: FirebaseAuth;
+    private lateinit var auth: FirebaseAuth
+
+    private var email: String = ""
+    private var password: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,12 +34,39 @@ class LoginActivity : AppCompatActivity() {
         }
         auth = Firebase.auth
 
+        val loginBtn = findViewById<MaterialButton>(R.id.login_button)
+
         findViewById<MaterialButton>(R.id.signup_link).setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
         }
 
+        findViewById<AppCompatEditText>(R.id.email_input).apply {
+            hint = "Email"
+            addTextChangedListener {
+                email = it.toString()
+                loginBtn.isEnabled =
+                    email.isNotEmpty() && password.isNotEmpty()
+            }
+        }
+
+        findViewById<AppCompatEditText>(R.id.password_input).apply {
+            hint = "Password"
+            addTextChangedListener {
+                password = it.toString()
+                loginBtn.isEnabled =
+                    email.isNotEmpty() && password.isNotEmpty()
+            }
+        }
+
         findViewById<MaterialButton>(R.id.login_button).setOnClickListener {
-            // TODO: Some checking
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        startActivity(Intent(this, ManagementFoodActivity::class.java))
+                    } else {
+                        Toast.makeText(this, "Authentication failed.", Toast.LENGTH_LONG).show()
+                    }
+                }
         }
 
     }
--- app/src/main/java/com/example/foodOrderAndTrackingApp/MainActivity.kt
+++ app/src/main/java/com/example/foodOrderAndTrackingApp/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.foodOrderAndTrackingApp
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.button.MaterialButton
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +19,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        auth = Firebase.auth
+
+        if (auth.currentUser != null) {
+            startActivity(Intent(this, ManagementFoodActivity::class.java).apply {
+                this.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            })
+        } else {
+            Toast.makeText(this, "Login not ed", Toast.LENGTH_LONG).show()
+        }
+
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -32,15 +46,5 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-
-        if(auth.currentUser != null){
-
-        } else {
-
-        }
-
-    }
 
 }
