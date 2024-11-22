690d458d404258aa948054d39a3958038976f7ba
Leo Wu <leo.wu.kw@gmail.com>
2024-11-23 00:24:32 +0800

    setup google map

--- app/src/main/java/com/example/foodOrderAndTrackingApp/activity/DeliveryTrackingActivity.kt
+++ app/src/main/java/com/example/foodOrderAndTrackingApp/activity/DeliveryTrackingActivity.kt
@@ -6,8 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.foodOrderAndTrackingApp.R
+import com.google.android.gms.maps.CameraUpdate
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.button.MaterialButton
 
-class DeliveryTrackingActivity : AppCompatActivity() {
+class DeliveryTrackingActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +26,31 @@ class DeliveryTrackingActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        findViewById<MaterialButton>(R.id.btn_back).setOnClickListener {
+            finish()
+        }
+
+        val mapFragment = SupportMapFragment.newInstance()
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.mapContainer, mapFragment)
+            .commit()
+        mapFragment.getMapAsync(this);
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        val latLngMuhk = LatLng(22.316372, 114.180087)
+        googleMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(latLngMuhk)
+                .title("MUHK")
+        )
+
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(18f))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLngMuhk))
+        googleMap.isTrafficEnabled = true
     }
 }
